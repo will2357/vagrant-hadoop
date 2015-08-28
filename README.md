d687e60aa34394a1d9b450b3019099002776aa0a
Will Highducheck <will@adaptly.com>
2015-08-27 23:17:41 -0700

    Hadoop Single Node Cluster

--- README.md
+++ README.md
@@ -4,6 +4,48 @@ A Vagrant virtual environment configured with Hadoop 2.7 running on Oracle Java 
 
 ## Installation
 
-1. Install [VirtualBox](http://www.virtualbox.org/).
+1. Install [VirtualBox](http://www.virtualbox.org/)
 1. Install [Vagrant](http://www.vagrantup.com/downloads)
 1. Run `vagrant up` (This may take a while the first time)
+
+Confirm that the installation is working and port forwarding is correctly
+configured by loading [http://127.0.0.1:4567/test.html](http://127.0.0.1:4567/test.html) in your browser.
+
+## Use
+
+Connect to the machine with `vagrant ssh`
+Confirm that Hadoop is running on Java 7 with `hadoop version`
+
+You should now have access to the following commands:
+* `container-executor`
+* `hadoop`
+* `hdfs`
+* `mapred`
+* `rcc`
+* `test-container-executor`
+* `yarn`
+
+And the following scripts:
+* `distribute-exclude.sh`
+* `hadoop-daemon.sh`
+* `hadoop-daemons.sh`
+* `hdfs-config.sh`
+* `httpfs.sh`
+* `kms.sh`
+* `mr-jobhistory-daemon.sh`
+* `refresh-namenodes.sh`
+* `slaves.sh`
+* `start-all.sh`
+* `start-balancer.sh`
+* `start-dfs.sh`
+* `start-secure-dns.sh`
+* `start-yarn.sh`
+* `stop-all.sh`
+* `stop-balancer.sh`
+* `stop-dfs.sh`
+* `stop-secure-dns.sh`
+* `stop-yarn.sh`
+* `yarn-daemon.sh`
+* `yarn-daemons.sh`
+
+Note that these and the rest of Hadoop are located in `/usr/local/hadoop`
---
+++ bootstrap.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+JAVA_HOME_DIR=/usr/lib/jvm/java-7-oracle/
+HADOOP_VERSION=hadoop-2.7.1
+HADOOP_FILE="$HADOOP_VERSION".tar.gz
+HADOOP_MIRROR=http://mirrors.gigenet.com/apache/hadoop/common/"$HADOOP_VERSION"/"$HADOOP_FILE"
+HADOOP_LOCATION=/usr/local/hadoop
+
+
+#################
+# Oracle Java 7 #
+#################
+add-apt-repository -y ppa:webupd8team/java
+apt-get update
+echo "export JAVA_HOME=$JAVA_HOME_DIR" >> /etc/environment
+echo debconf shared/accepted-oracle-license-v1-1 select true | \
+  debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | \
+  debconf-set-selections
+apt-get install -y oracle-java7-installer
+#################
+
+
+#########################################
+# To confirm port forwarding is working #
+#########################################
+#http://127.0.0.1:4567/test.html
+apt-get install -y apache2
+if ! [ -L /var/www ]; then
+  rm -rf /var/www
+  ln -fs /vagrant /var/www
+fi
+mkdir -p /vagrant/html/
+echo HI > /vagrant/html/test.html
+#########################################
+
+
+##############################
+# Hadoop Single Node Cluster #
+##############################
+apt-get install -y ssh
+apt-get install -y rsync
+
+if [ -f /vagrant/$HADOOP_FILE ] && [ ! -f $HADOOP_FILE ]
+then
+  cp /vagrant/$HADOOP_FILE .
+elif [ ! -f $HADOOP_FILE ]
+then
+  wget $HADOOP_MIRROR
+fi
+tar -xzf $HADOOP_FILE
+mv $HADOOP_VERSION $HADOOP_LOCATION
+chown -R vagrant $HADOOP_LOCATION
+
+echo "export HADOOP_HOME=$HADOOP_LOCATION" >> /etc/environment
+echo "export PATH=$PATH:$HADOOP_LOCATION/bin:$HADOOP_LOCATION/sbin" >> /etc/environment
+echo "export HADOOP_PREFIX=$HADOOP_LOCATION" >> /etc/environment
+##############################
